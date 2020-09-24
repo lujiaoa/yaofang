28840b352bdda4b52a54679784b238b8802feecc
lujiaoa <2607330608@qq.com>
2020-09-24 19:24:43 +0800

    首页部分还差一点

---
+++ js/login.js
@@ -0,0 +1,27 @@
+window.addEventListener("load", () => {
+  // 获取记住密码的div
+  let ckdiv = document.querySelector(".ckdiv");
+  // console.log(ckdiv)
+  // 获取i元素
+  let checked = document.querySelector(".ckbox>i")
+  // console.log(checked)
+  ckdiv.onclick=function(){
+    // 如果i上面有类名就清除类名，没有就添加
+    if(checked.className){
+      checked.className=""
+    }else{
+      checked.className="checked"
+    }
+  }
+  // 用于切换表头
+  let item = document.querySelectorAll(".item");
+  // console.log(item)
+  item.forEach((item)=>{
+    // item.className="item"
+    item.onclick=()=>{
+      item.
+      item.className="item selected";
+      
+    }
+  })
+})
